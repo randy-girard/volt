b4ef67bdfbc6711f432a9d2b6d317c1fc3019ac4
Randy Girard <randy@tradepending.com>
2023-08-19 11:22:25 -0400

    Update some settings with log file reading

--- volt/utils/log_reader.py
+++ volt/utils/log_reader.py
@@ -5,7 +5,6 @@ import select
 import sys
 
 from volt.utils.log_watcher import LogWatcher
-
 from pubsub import pub
 from threading import Thread
 from threading import Event
@@ -14,16 +13,18 @@ class LogReader():
     def __init__(self):
         self.running = Event()
         self.thread = None
+        self.watcher = None
+        self.watching = True
 
     def start(self):
         self.running.set()
-
         self.thread = Thread(target=self.run, args=(self.running,))
         self.thread.start()
 
-
     def stop(self):
         self.running.clear()
+        if self.watcher:
+            self.watcher.close()
         if self.thread:
             self.thread.join()
 
@@ -32,24 +33,21 @@ class LogReader():
 
     def run(self, running):
         if os.path.isfile(self.log_file):
-            watcher = LogWatcher(".", self.callback, ["log"],
-                         self.log_file,
-                         persistent_checkpoint=False,
-                         mask_rotated_file_name=False,
-                         deterministic_rotation=False,
-                         file_signature_bytes=32,
-                         strict_extension_check=True)
+            self.watcher = self.init_tail_file(self.log_file)
             while running.is_set():
-                watcher.loop(blocking=False)
+                self.watcher.loop(blocking=False)
                 time.sleep(0.1)
-        #tail = Tail(self.log_file)
-        #tail.register_callback(self.emit)
-        #tail.follow(s=0.1, run=running)
-
-
-    #def emit(self, text):
-    #    pub.sendMessage('log', text=text.strip("\n"))
 
     def callback(self, filename, lines):
         for line in lines:
+            print(line.strip("\n"))
             pub.sendMessage('log', text=line.strip("\n"))
+
+    def init_tail_file(self, filename):
+        return LogWatcher(".",
+                          self.callback,
+                          ["log"],
+                          filename,
+                          persistent_checkpoint=False,
+                          file_signature_bytes=32,
+                          strict_extension_check=True)

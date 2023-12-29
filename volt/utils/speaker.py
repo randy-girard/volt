65c3ada9e21d9923050b7ccad53b99957e49c998
Randy Girard <rgirard59@yahoo.com>
2023-12-29 09:43:34 -0500

    Update speech engine so that it doesn't steal focus on windows

--- volt/utils/speaker.py
+++ volt/utils/speaker.py
@@ -1,21 +1,61 @@
 import platform
-import subprocess
+import pyttsx3
+
+from threading import Thread
+
+if platform.system() == "Darwin":
+    import subprocess
+elif platform.system() == "Windows":
+    from threading import Thread
+    from pyttsx3.drivers import toUtf8, fromUtf8
+
+    def _say_stopable(engine, text):
+        engine.say(text)
+        engine.runAndWait()
+
+
+class Threader(Thread):
+    def __init__(self, *args, **kwargs):
+        Thread.__init__(self, *args, **kwargs)
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        if platform.system() == "Windows":
+            self._args[0].proxy.setBusy(True)
+            self._args[0].proxy.notify('started-utterance')
+            self._args[0]._speaking = True
+            self._args[0].proxy._driver._tts.Speak(fromUtf8(toUtf8(self._args[1])), 3)
+        elif platform.system() == "Darwin":
+            pass
+
 
 class Speaker():
     def __init__(self):
         self.proc = None
-        
+        self.engine = None
+
+        self.driver_name = 'sapi5'
         if platform.system() == "Darwin":
-            pass
-        else:
-            pass
+            self.driver_name = 'nsss'
+
+        self.engine = pyttsx3.init(driverName=self.driver_name)
 
     def say(self, text):
-        if platform.system() == "Darwin":
-            self.proc = subprocess.Popen("say \"" + text + "\"", shell=True)
-        else:
-            self.proc = subprocess.Popen(f"mshta vbscript:Execute(\"CreateObject(\"\"SAPI.SpVoice\"\").Speak(\"\"{text}\"\")(window.close)\")")
+        if self.engine:
+            self.stop()
+
+            if platform.system() == "Darwin":
+                self.proc = subprocess.Popen("say \"" + text + "\"", shell=True)
+            elif platform.system() == "Windows":
+                self.proc = Threader(args=(self.engine, text))
+
 
     def stop(self):
-        if self.proc:
-            self.proc.terminate()
+        if platform.system() == "Darwin":
+            if self.proc:
+                self.proc.terminate()
+        elif platform.system() == "Windows":
+            if self.engine:
+                self.engine.proxy._driver._tts.Skip('Sentence', 2147483647)
+                self.engine._inLoop = False
